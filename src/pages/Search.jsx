33b0bcc85345f88e91091c6d13b10f98c3bb06ff
yeddaTech <esyoun70@gmail.com>
2026-04-03 15:30:00 +0200

    fix: rimosso import Home.css da Search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useSearchParams, useNavigate } from 'react-router-dom';
-import './Home.css'; // Usiamo il CSS della Home per avere lo stile del search-box
 
 export default function Search() {
   const navigate = useNavigate();
