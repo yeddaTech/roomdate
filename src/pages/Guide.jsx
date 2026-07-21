81ac91f83f1a01de05bacd75ac957f9598dfe191
yeddaTech <esyoun70@gmail.com>
2026-07-21 15:14:09 +0200

    fix: corretto percorso import Footer in Home

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
-import Footer from './Footer';
+import Footer from '../components/Footer';
 
 export default function Guide() {
   // --- INIZIO SCHEMA MARKUP (La magia per la SEO e le Intelligenze Artificiali) ---
