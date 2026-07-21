f2ae085d9752cbe553d92b12e51d077a0a02357a
yeddaTech <esyoun70@gmail.com>
2026-07-21 12:50:54 +0200

    fix: corretto percorso import Footer in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import Footer from './Footer';
+import Footer from '../components/Footer';
 export default function Home() {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
