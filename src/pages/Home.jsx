33d618fedabd7cecc4211a82c9bdda21caaf4fb5
yeddaTech <esyoun70@gmail.com>
2026-07-23 15:52:41 +0200

    ottimizzazioni google search control

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'sreact';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Footer from '../components/Footer';
 export default function Home() {
